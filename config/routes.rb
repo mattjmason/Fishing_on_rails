72b82d40c0797bd6ad791b8e7432aaf054c9343f
mattjmason <masonmj10@gmail.com>
2021-08-03 12:14:05 -0400

    models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :categories
+  resources :locations
+  resources :fish
   resources :anglers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
